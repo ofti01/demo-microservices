4ebe726be1df9b954caecdef9c1c350a4e9eb0a1
Lotfi <oftizeidi@gmail.com>
2022-10-21 11:46:17 +0100

    add module invoice with success and configure pom.xml

---
+++ invoice-microservices/src/main/java/com/lotfi/invoicemicroservices/InvoiceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.lotfi.invoicemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceMicroservicesApplication.class, args);
+    }
+
+}
